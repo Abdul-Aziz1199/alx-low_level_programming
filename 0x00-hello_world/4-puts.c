fa4f79a12f70d22be6d8548d1af4ec624b84933d
Abdul-Aziz1199 <abdulalhassan1199@gmail.com>
2022-12-08 17:41:22 -0800

    puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-print a line of code using puts
+ *
+ *Description:A C program that print with exectly
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like buildimg a multilingual puzzle");
+	return (0);
+
+}
