e98b20ae8f49d96887d082ce621c82ca0898b06e
Abdul-Aziz1199 <abdulalhassan1199@gmail.com>
2022-12-16 15:13:44 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
-
+int main (void);
 int _isupper(int c) {
 
 char ch = 'A';
@@ -8,7 +8,7 @@ char ch = 'A';
 		printf("\nEntered character is upper case");
      else
 	     printf("\nEntered character is not upper case");
-      return(1);    
+      return(0);    
 
 
 
