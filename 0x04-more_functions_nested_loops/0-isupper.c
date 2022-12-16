676d416da010f7024733d5ecbf0a638733e91df3
Abdul-Aziz1199 <abdulalhassan1199@gmail.com>
2022-12-16 14:07:39 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int _isupper(int c) {
+
+char ch = 'A';
+	if (isupper (ch))
+		printf("\nEntered character is upper case");
+     else
+	     printf("\nEntered character is not upper case");
+      return(1);    
+
+
+
+
+}
