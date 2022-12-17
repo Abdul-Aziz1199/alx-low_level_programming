f2e805f8fd2fbd6afda82c0da0305e842908c60d
Abdul-Aziz1199 <abdulalhassan1199@gmail.com>
2022-12-17 02:57:12 -0800

    negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
-int main() 
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * main - main block
+ *
+ *   * Description: Get a ramdon number and print the number
+ *
+ *    * and if it is positive, negative, or zero
+ *
+ *     * Return: 0
+ *
+ *      */
+
+int main(void)
+
 {
 
-	int n;
-	for ( n =0; n < 5; n++) {
-	
-         printf("%d\n", n);
-
-	 getchar();
+ int n;
 
 
 
+srand(time(0));
 
-	}	 
-
-
-
+n = rand() - RAND_MAX / 2;
 
 
 
+    if (n > 0)
 
-}		
+	printf("%i is positive\n", n);
+
+    else if (n < 0)
+
+	printf("%i is negative\n", n);
+
+    else
+
+										       printf("%i is zero\n", n);
+
+	return (0);
+
+										  if (n > 0)
+
+										     printf("%i is positive\n", n);
+
+										  else if (n < 0)
+
+																				    printf("%i is negative\n", n);
+
+										  else
+
+																				   printf("%i is zero\n", n);
+
+																					return (0);
+
+}
